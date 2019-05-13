f29ba45ab2c36f725f2781baba20606eb1086ad9
nzbin <nzb329@163.com>
2019-05-13 13:06:04 +0800

    refator: modified types file

---
+++ types/index.d.ts
@@ -0,0 +1,98 @@
+declare namespace PhotoViewer {
+  enum toolbarOptions {
+    Maximize = 'maximize',
+    Close = 'close',
+    ZoomIn = 'zoomIn',
+    ZoomOut = 'zoomOut',
+    Prev = 'prev',
+    Fullscreen = 'fullscreen',
+    Next = 'next',
+    ActualSize = 'actualSize',
+    RotateRight = 'rotateRight'
+  }
+
+  type toolbarOption =
+    | 'maximize'
+    | 'close'
+    | 'zoomIn'
+    | 'zoomOut'
+    | 'prev'
+    | 'fullscreen'
+    | 'next'
+    | 'rotateRight'
+    | 'rotateLeft'
+    | 'actualSize';
+
+  export interface Img {
+    src: string;
+    title?: string;
+  }
+
+  export interface Options {
+    draggable?: boolean;
+    resizable?: boolean;
+    movable?: boolean;
+    keyboard?: boolean;
+    title?: boolean;
+    modalWidth?: number;
+    modalHeight?: number;
+    fixedContent?: boolean;
+    fixedModalSize?: boolean;
+    initMaximized?: boolean;
+    gapThreshold?: number;
+    ratioThreshold?: number;
+    minRatio?: number;
+    maxRatio?: number;
+    headToolbar?: toolbarOption[] | string[];
+    footToolbar?: toolbarOption[] | string[];
+    icons?: {
+      minimize?: string;
+      maximize?: string;
+      close?: string;
+      zoomIn?: string;
+      zoomOut?: string;
+      prev?: string;
+      next?: string;
+      fullscreen?: string;
+      actualSize?: string;
+      rotateLeft?: string;
+      rotateRight?: string;
+    };
+    i18n?: {
+      minimize?: string;
+      maximize?: string;
+      close?: string;
+      zoomIn?: string;
+      zoomOut?: string;
+      prev?: string;
+      next?: string;
+      fullscreen?: string;
+      actualSize?: string;
+      rotateLeft?: string;
+      rotateRight?: string;
+    };
+    multiInstances?: boolean;
+    initAnimation?: boolean;
+    fixedModalPos?: boolean;
+    zIndex?: number;
+    dragHandle?: boolean;
+    callbacks?: {
+      beforeOpen?: Function;
+      opened?: Function;
+      beforeClose?: Function;
+      closed?: Function;
+      beforeChange?: Function;
+      changed: Function;
+    };
+    index?: number;
+    progressiveLoading?: boolean;
+  }
+}
+
+declare class PhotoViewer {
+  constructor(items: PhotoViewer.Img[], options?: PhotoViewer.Options);
+}
+
+declare module 'photoviewer' {
+  export default PhotoViewer;
+}
